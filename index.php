685b4a59ff21b8cdfadf882fc6d051506c5e1730
Bryan-Cardenas1 <bryan.cardenas@alumnos.upla.cl>
2024-11-06 21:59:50 -0300

    V3

--- index.php
+++ index.php
@@ -10,21 +10,27 @@
 <body>
     <section class="flex-container">
     <div class="form-style">
-        <h5>Registro</h5>
-        <form action="procesar.php" method="GET" enctype="multipart/form-data">
-        <label for="name">Nombre</label>
-        <input type="text" id="name" name="name" placeholder="Ingrese su Nombre">  
-        
-        <label for="rut">Rut</label>
-        <input type="text" id="rut" name="rut" placeholder="Ingrese su Rut">  
+        <h5>Registro Asistentes</h5>
+        <form action="procesar.php" method="POST" enctype='multipart/form-data'>
+            
+            <label for="name">Nombre</label>
+            <input type="text" id="name" name="name" placeholder="Ingrese su Nombre">  
+            
+            <label for="rut">Rut</label>
+            <input type="text" id="rut" name="rut" placeholder="Ingrese su Rut">  
 
-        <label for="email">Apellido</label>
-        <input type="email" id="email" name="email"placeholder="Ingrese su Email">  
-        <label for="phone">Telefono</label>
-        <input type="text" id="phone" name="phone"placeholder="Ingrese su Numero">
-        <input type="file" name="image" id="image" placeholder="Suba su imagen">
-       <button type="submit">Enviar</button>
-    
+            <label for="email">Apellido</label>
+            <input type="email" id="email" name="email"placeholder="Ingrese su Email">  
+
+            <label for="phone">Telefono</label>
+            <input type="text" id="phone" name="phone"placeholder="Ingrese su Numero">
+
+            <input type="file" name="image" id="image" required>
+            <button type="submit">Enviar</button>
+
+        </form>
+        
+
     </div>
 
     </section>
--- procesar.php
+++ procesar.php
@@ -1,27 +1,38 @@
 <?php
-   if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    $name=$_GET["name"];
-    $rut=$_GET["rut"];
-    $email=$_GET["email"];
-    $phone=$_GET["phone"];
-    $image=$_FILES["image"];
-    $text = "Name: $name\nRUT: $rut\nEmail: $email\nPhone: $phone\nImagen: $imagePath";
-    QRcode::png($text, 'filename.png');
+   require_once('phpqrcode/qrlib.php');
 
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      $name=$_POST["name"];
+      $rut=$_POST["rut"];
+      $email=$_POST["email"];
+      $phone=$_POST["phone"];
 
-    
-    $conn = mysqli_connect($servername, $username, $password);
 
-    if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
+      //$imageName = $_FILES['image']['name']; 
+      $imageTmpPath = $_FILES['image']['tmp_name'];
+   
+      $text = "Name: $name\nRUT: $rut\nEmail: $email\nPhone: $phone";
 
+      // Nombre del archivo para guardar el QR
+      $filename = 'qrcode_.png';
+
+      // Generar el código QR
+      QRcode::png($text);
+
+      /*$servername = "localhost";
+      $username = "username";
+      $password = "password";
+
+      
+      $conn = mysqli_connect($servername, $username, $password);
+
+      if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+      }
+      echo "Connected successfully";
+
+      */
    }
-
   
 ?>
 
